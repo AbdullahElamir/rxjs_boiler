549c7ffd3a010613278baf34bd6f906fb0d58eb1
abdullah <abdullah.el_ameer@yahoo.com>
2017-02-27 14:37:56 +0200

    all

--- src/app.js
+++ src/app.js
@@ -1,4 +1,22 @@
 import $ from 'jquery';
 import Rx from 'rxjs/Rx';
 
-console.log('RxJS Boiler Running...');
+
+// *******************  Observable fromEvent ************
+
+
+
+// *********************** Click Event ***********************
+const btn = $('#btn');
+const btnStream$ = Rx.Observable.fromEvent(btn,'click');
+btnStream$.subscribe(function(e){
+	console.log(e.target.value);
+
+	output.append(e.target.value+" ---- ");
+},function(err){
+	console.log(err);
+},function(){
+	console.log("completed");
+})
+
+// ************************************************************
